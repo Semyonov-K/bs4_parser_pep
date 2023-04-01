0534b9c7dbbde53a38d23e78bd6618b25741d70f
unknown <fixer20112@mail.com>
2023-04-01 20:11:17 +0300

    logging, count result

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import logging
 import re
 from urllib.parse import urljoin
+from collections import defaultdict
 
 import requests_cache
 from bs4 import BeautifulSoup
@@ -13,42 +14,45 @@ from utils import find_tag, get_response
 
 
 result_of_status = {}
+messages_of_warnings = []
 
 
 def pep(session):
-    pep_url = PEP_URL
-    response = get_response(session, pep_url)
+    response = get_response(session, PEP_URL)
     if response is None:
         return
     soup = BeautifulSoup(response.text, features='lxml')
     result = find_tag(soup, 'section', attrs={'id': 'numerical-index'})
     tag_abbr = result.find_all('abbr')
+    logging.info('Старт парсера страниц.')
     for tag in tqdm(tag_abbr):
         status = tag.text[1:]
         sibling = tag.parent.find_next_sibling()
         tag_a = find_tag(sibling, 'a')
         link = tag_a['href']
         one_page(session, status, link)
+    logging.warn('\n'.join(messages_of_warnings))
     result_of_status['total'] = sum(result_of_status.values())
-    return result_of_status
+    total_result = [[key, value] for key, value in result_of_status.items()]
+    return total_result
 
 
 def one_page(session, status, link):
-    logging.info('Парс одной страницы.')
     url_link = urljoin(PEP_URL, link)
     response = get_response(session, url_link)
     if response is None:
         return
     soup = BeautifulSoup(response.text, features='lxml')
-    result = soup.find(string='Status')
+    result = find_tag(soup, string='Status')
     status_sibling = result.parent.next_sibling.next_sibling.text
     if status_sibling not in result_of_status:
-        result_of_status[status_sibling] = 0
-    result_of_status[status_sibling] += 1
+        result_of_status[status_sibling] = result_of_status.get(status_sibling, 0) + 1
+    else:
+        result_of_status[status_sibling] += 1
     if status_sibling not in EXPECTED_STATUS.get(status):
-        logging.warn(
-            f'Несовпадающие статусы: {url_link}'
-            f'Статус в карточке: {status_sibling}'
+        messages_of_warnings.append(
+            f'Несовпадающие статусы: {url_link} '
+            f'Статус в карточке: {status_sibling} '
             f'Ожидаемые статусы: {EXPECTED_STATUS[status]}'
         )
     return
@@ -154,8 +158,6 @@ def main():
     parser_mode = args.mode
     results = MODE_TO_FUNCTION[parser_mode](session)
     if results is not None:
-        if parser_mode == 'pep':
-            results = [[key, value] for key, value in results.items()]
         control_output(results, args)
     logging.info('Парсер завершил работу.')
 
